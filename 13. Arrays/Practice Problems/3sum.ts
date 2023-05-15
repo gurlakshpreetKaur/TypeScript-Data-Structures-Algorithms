8c5ae8451164c3ecc75aab7011493ff3e6e40bdf
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2023-05-15 23:52:13 +0530

    add 2Sum sorted and 3Sum solutions

---
+++ 13. Arrays/Practice Problems/3sum.ts
@@ -0,0 +1,104 @@
+//Q:Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and 
+//  j != k, and nums[i] + nums[j] + nums[k] == 0. Notice that the solution set must not contain duplicate triplets.
+//
+//First, lets understand this question. We will be given an integer array nums. We need to write an algorithm to
+//  find all triplets such that they add up to 0, and return them in form of a 2D array.
+// But, we cannot repeat items which have the same index.
+//For example-
+//      nums = [-1, 2, 0, -2, 4, -2]
+//
+//      Here, [-1, -1, 2] is NOT ACCEPTABLE, because -1 occurs once in the array at index 0, and we cannot repeat
+//              elements with the same array.
+//      But [-2, 4, -2] is acceptable because -2 occurs twice in the array, so -2 is not repeated from the same
+//              index, since is occurs at difference indices in the array.
+//Also, we cannot have duplicate pairs.
+//      nums = [-1, 2, 0, -2, 4, -2]
+//
+//      Here, we cannot include both [-2, 4, -2] and [-2, -2, 4] in the answer array. The solutions need to be 
+//          unique.
+//
+//There are many ways to solve this question. The simplest is to use 3 nested loops, you could try that but this
+//  solution does not cover that approach.
+//2 very popular methods are to use a Map, or to use a 2 pointer approach.
+//
+//This solution covers the 2 pointer approach. To understand what the 2 pointer apporach is, please look at the
+//  solution for the 2sum problem.
+//
+//
+//
+//For this question, we use a variation of the 2-pointer approach-
+// Step 1: Declare 2D array sol, and initialize it with an empty array.
+// Step 2: sort nums using inbuilt sort. this is very important because it helps us easily compare elements to prevent
+//             dupicates.
+// Step 3: create a loop from 0 to length-1. in each iteration, we will take i as a fixed point, and use it to compare
+//              if the other elements and ith element together can add up to 0.
+// For further explanation, please refer to the code.
+
+function threeSum(nums: number[]): number[][] {
+    //create 2D array sol and initialize as an empty array
+    let sol: number[][] = [];
+    //sort nums using inbuilt sort
+    nums.sort((a, b) => a - b);
+
+    //since nums is sorted, if the first element is greater than 0, we know that no negative number exists in the
+    //array. This means there CANNOT exist a set of 3 numbers such that they add up to 0, because we need a -ve
+    //number to cancel out the +ve so that we can end up with a 0. so, we return an empty array, as no solution
+    //exists
+    if (nums[0] > 0) return [];
+    for (let i = 0; i < nums.length - 2; i++) {
+        //if curernt element equals previous element, this means we're at a duplicate element. since the array is
+        //sorted, we can be sure that if we skip the iteration each time nums[i] == nums[i - 1], we can eliminate
+        //the duplicate fixed numbers.
+        if (nums[i] == nums[i - 1]) continue;
+
+        //starting 2 pointer approach from i + 1
+        let start = i + 1;
+        //declaring end variable as same each time as the array is sorted
+        let end = nums.length - 1;
+
+        //while start < end to use the two pointer approach
+        while (start < end) {
+            //calculate sum of fixed number, start element, end end element
+            let sum = (nums[i] + nums[start] + nums[end]);
+
+            //checking is sum equals 0
+            if (sum === 0) {
+                //if sum equals 0, add it to solution array
+                sol.push([nums[i], nums[start], nums[end]]);
+
+                //store current start element in an array
+                let currentStartElement = nums[start];
+                //while element at index start equal currentStartElement, we increment the start element.
+                //this is done to eliminate duplicates that may arise from the start variable. there can be multiple
+                //equal numbers in a row, so this is necessary
+                while (nums[start] === currentStartElement && start < end) {
+                    //moving towards the right, away from the initial start point
+                    start++;
+                }
+
+                //repeating same procedure as with the start element
+                let currentEndElement = nums[end];
+                while (nums[end] === currentEndElement && start < end) {
+                    //but here we move towards the left, away from the initial end point
+                    end--;
+                }
+            }
+            //if sum<0, that means we need to increase the sum. to do this we increase start index (look at sorted
+            //sum solution for more details)
+            else if (sum < 0) {
+                //incrementing start index
+                start++;
+            }
+            //if this piece of code is executed, that means sum>0, in this case we want to decrease the sum. to do
+            //this, we do end--.(look at sorted sum solution for more details)
+            else {
+                end--;
+            }
+        }
+    }
+    //return sol
+    return sol;
+}
+
+//TC: O(n^2)   (2 pointer approach takes O(n), and we out that in the for loop, so we get O(n^2))
+//SC: O(n)     (we declared the sol array, which can vary depending on the input array)
---
+++ 13. Arrays/Practice Problems/sorted2Sum.ts
@@ -0,0 +1,121 @@
+//Q:Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers 
+//such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] 
+//where 1 <= index1 < index2 <= numbers.length. Return the indices of the two numbers, index1 and index2, added by 
+//one as an integer array [index1, index2] of length 2. The tests are generated such that there is exactly one 
+//solution. You may not use the same element twice. Your solution must use only constant extra space.
+//
+//Let's first understand the question. We will be given an array of integers, which is 1-INDEXED, which means, we
+// start indexing from 1 and not 0. We are tasked with returning the indices of a pair of two numbers from the array
+// which add up to a given target BUT we can only use constant extra space.
+//
+//Intuitively, you might have thought of using a nested loop, which i as outer loop and j as inner loop. You could
+// code it out, but we will not be using that approach, as it has a TC of O(n^2), which is very slow.
+//
+//Instead, we are going to the a 2-pointer approach. 2-pointer approach is a pretty popular way of handling
+// 2 variables in a single loop. As the name suggests, we create 2 pointers, which point to different indices, and
+// use them with a loop to solve the problem. 
+//
+//First, loop at this simple observation-
+// Since the question says is is GUARANTEED that there exists a solution, and also that the array is sorted, we
+// know that if the smaller number is at index i, the other number will be at an index greater than i.
+//
+//Now, to understand the 2-pointer approach, look at the following example solved using the
+// 2-pointer approach-
+//          arr = [1, 2, 3, 4, 5, 6, 7, 8], target = 10
+//  The first step is to create the pointers
+//          we will create 2 variables- start and end
+//          start will be set to 0, and end will be set to arr.length - 1.
+//      In case of this array,
+//          start = 0
+//          end = arr.length-1 = 7-1 = 6
+//  Now, we create a while loop, which runs while start < end  (this is important, since we want both variables to
+//    have a value. and as seen in the observation, one of the indexes will always be greater than the other if there
+//    exists a valid solution)
+//  In this while loop, we calculate the sum of the items at the start and end index.
+//
+//           s = start, e = end
+//          [1, 3, 4, 5, 6, 7, 8]
+//           s                 e
+//         sum = nums[s]+nums[e] = 1+8 = 9
+//
+//     Now, we compare the sum with the target
+//          if sum===target, return [start + 1, end + 1] as answer. Why do we add 1? because the array is 1-INDEXED,
+//                                              but we solved the problem from a 0 index point of view, so we add 1
+//          else if sum>target, end--    Why? Because the end element is the greatest out of our pair, so if the sum
+//                                        is greater than the target, we decrease the greater index. Why not
+//                                        decrease start instead? 
+//                                        1. start is initialized with 0, so if we decrease it, start will become 
+//                                           negative, which will lead to an error when we try to reference that 
+//                                           index from the array. 
+//                                        2. as you will later see, we do increase start, but that is if sum IS 
+//                                           GREATER than target. if we decrease start when sum IS LESS than target, 
+//                                           we are essentially going back to a case we have already seen, since we
+//                                           already came from the left direction, going back there and expecting a 
+//                                           different outcome is illogical.
+//          else start++;     if this else statement was executed, that means that sum<target. since start element
+//                                        is the smaller member of our pair, we increase it, so that sum will also
+//                                        increase. Why not increase end instead? 
+//                                        1. end is initialized with length-1, so if we decrease it, start will 
+//                                           become out of bound, which will lead to an error when we try to  
+//                                           reference that index from the array. 
+//                                        2. as you have seen we do increase end, but that is if sum IS LESSER than 
+//                                           target. if we increase end when sum IS GREATER than target, we are 
+//                                           essentially going back to a case we have already seen, since we already
+//                                           came from the right direction, going back there and expecting a  
+//                                           different outcome is illogical.
+//
+//          In our case, 9<10, which means sum<target, so we do start++.
+//          [1, 3, 4, 5, 6, 7, 8]
+//           s                 e      BEFORE
+//              s              e      AFTER
+//
+//      Now we repeat this process over and over again until we either find start and end such that arr[start]+arr[end]
+//          equals target, or start becomes >= end in which case the loop breaks.
+//
+//          [1, 3, 4, 5, 6, 7, 8]
+//              s              e      CURRENT
+//          sums = nums[s]+nums[e] = 3+8 = 11
+//              11>10, which means sum>target, so we do end--
+//          [1, 3, 4, 5, 6, 7, 8]
+//              s              e      BEFORE
+//              s           e         AFTER
+//              
+//          [1, 3, 4, 5, 6, 7, 8]
+//              s           e      CURRENT
+//          sums = nums[s]+nums[e] = 3+7 = 10
+//              10===10, so we return [start + 1, end + 1]. start = 1, end = 5, so [start + 1, end + 1] equals
+//                  [1 + 1, 5 + 1] = [2, 6]. Remember, we added 1 because the question specifies that this array
+//                  is 1-INDEXED. Hence, [2, 6] is our answer.
+//
+function twoSum(numbers: number[], target: number): number[] {
+    //declare start var and initialize with 0
+    let start = 0;
+    //declare end var and initialize with numsber.length-1
+    let end = numbers.length - 1;
+
+    //create a loop that will run while start < end
+    while (start < end) {
+        //calculate sum of numbers at indices start and end
+        let sum = numbers[start] + numbers[end];
+
+        //if sum===target, it means we found the target, so we return [start + 1, end + 1]. we return this because
+        //the question specified that we had to return an array containing the indices of the 2 numbers, and we
+        //add 1 because the question specified we had to return the indices assuming they were from a 1-INDEXED
+        //array. we wrote the solution according to a 0-INDEXED array, so need to adjust it accordingly by adding 1.
+        if (sum === target) return ([start + 1, end + 1]);
+
+        //if sum<target, we decrease end by 1. since the array is non decreasingly sorted, this means the next 
+        //iteration will have a smaller sum, which is what we need since sum>target.
+        if (sum > target) end--;
+
+        //if sum>target, we increase start by 1. since the array is non decreasingly sorted, this means the next 
+        //iteration will have a greater sum, which is what we need since sum<target.
+        else start++;
+    }
+    //if we break out of loop, it means no result was found. in this case we return [-1, -1] since this is an error
+    //case, as the question GUARANTEED that there would be a solution.
+    return [-1, -1]
+};
+
+//TC: O(n)   (the while loop depends on length of input)
+//SC: O(1)   (the amount of space taken up does not depend on length of input)
