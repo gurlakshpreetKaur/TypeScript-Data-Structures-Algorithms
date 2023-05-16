74640bb7c79ae063ec7bed8bca43ee5d74a276c3
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2023-05-16 14:10:03 +0530

    add bubble sort and selection sort

---
+++ 14. Basic Sorting Algorithms/bubbleSort.ts
@@ -0,0 +1,107 @@
+//Bubble sort is one of the most beginner-friendly sorting algorithms.
+//We'll be sorting in non-decreasing order.
+//It works by comparing adjacent elements, and moving greater elements towards the end. This way, at then end,
+//the greatest element will be last, the shortest element will be first in array, and the array will be sorted.
+//
+//In its implementation, a nested loop is used. To undestand how it works, loop at the following example-
+// arr = [3, 4, 2, 5, 1]
+// iteration = 0
+//  we will compare adject elements, starting from 0
+// arr = [3, 4, 2, 5, 1]
+//        j  k  
+//  is jth element > kth element? no, so we do nothing
+// arr = [3, 4, 2, 5, 1]
+//           j  k
+//  is jth element > kth element? yes, so we swap
+// arr = [3, 2, 4, 5, 1]
+//              j  k
+//  is jth element > kth element? no, so we do nothing
+// arr = [3, 2, 4, 5, 1]
+//                 j  k
+//  is jth element > kth element? yes, so we swap
+// arr = [3, 2, 4, 1, 5]
+//  observe that the largest element has been moved to the end in the 0th iteration. this is not a coincidence.
+//      in bubble sort, in each iteration we compare adjacent elements starting from 0, to length-iterationCount-1,
+//      and in each iteration, the last iterationCount + 1 elements will always be sorted, and we push greater
+//      elements towards the end.
+// iteration = 1
+// arr = [3, 2, 4, 1, 5]
+//        j  k
+//  is jth element > kth element, yes, so we swap
+//
+// arr = [2, 3, 4, 1, 5]
+//           j  k
+//  is jth element > kth element, no, so we do nothing
+//
+// arr = [2, 3, 4, 1, 5]
+//              j  k
+//  is jth element > kth element? yes, so we swap
+//
+// arr = [2, 3, 1, 4, 5]
+//  observe the last 2 elements are the 2 greatest numbers in that array, and they are sorted. our 1th iteration
+//       is also over.
+// iteration = 2
+// arr = [2, 3, 1, 4, 5]
+//        j  k
+//  is jth element > kth element? no, so we do nothing
+//
+// arr = [2, 3, 1, 4, 5]
+//           j  k
+//  is jth element > kth element, yes, so we swap
+//
+// arr = [2, 1, 3, 4, 5]
+//              j  k
+//  in this iteration, we don't need to compare, since we already know the last 2 numbers are the greatest
+//  iteration = 3
+// arr = [2, 1, 3, 4, 5]
+//        j  k
+//   is jth element > kth element? yes, so we swap
+//
+// arr = [1, 2, 3, 4, 5]
+//           j  k
+//  we dont need to compare now, since we already know the last 3 numbers are sorted (our iteration count is 3
+//  and bubble sort has this basic principle the in the ith count of the outer loop, the last i elements will
+//  always be sorted).
+//
+//Implementation-
+//Write nested loops with variables i and j. i will act as our outer iteration counter, which will help us know
+//  that the last i elements are sorted. we will use i to control the times that j runs, as we dont need to 
+//  compare last i elements, j will run arr.length-1-i times. this will avoid unecessary comparisions.
+//in the nested loop, we compare and swap adjacdent elements. so we compare j with j+1th element, and swap if
+//  necessary
+
+function bubbleSort(arr: number[]): number[] {
+    //create outer loop that goes from 0 to arr.length - 2, why -2? we could go to arr.length - 1, but we dont,
+    //because we know that by last iteration, the last ith numbers will be sorted. which means by last iteration,
+    //all numbers will be sorted except for 0th element of array. if we know all other numbers of array are greater
+    //than 0th element, then we dont even need to compare and check, because we can logically derive that if all
+    //other numbers are greater, than the 0th element MUST be smallest. so we go to arr.length-2 to avoid
+    //unecessary comparisions in that case.
+
+    for (let iterationCount = 0; iterationCount < arr.length - 1; iterationCount++) {
+        //inner loop which we will use to compare adjacent elements
+
+        for (let j = 0; j < arr.length - iterationCount - 1; j++) {
+
+            let k = j + 1; //j will be our first index, and k=j+1 will be the adjacent index.
+
+            if (arr[j] > arr[k]) {
+                //if the jth element > kth element, it means an item that occurs earlier in the array is greater
+                //than an item that occurs later in the array. since we are sorting in non-decreasing order,
+                //we will swap them so that the earlier elements are smaller than the later elements.
+                let jthElementCopy = arr[j];
+                arr[j] = arr[k];
+                arr[k] = jthElementCopy;
+
+            }
+        }
+
+        //this procedure will be repeated until the array is sorted.
+    }
+
+    //returning arr
+    return arr;
+}
+
+//TC: O(n^2)  (due to the nested loop which depend on the length of the input array)
+//SC: O(1)    (the variables which we declared do not depend on length of input array, as we swap IN PLACE)
---
+++ 14. Basic Sorting Algorithms/selectionSort.ts
@@ -0,0 +1,75 @@
+//selection sort is a beginner friendly and untuitive way of sorting an array.
+//most likely, if you've ever had to sort cards, you've used selection sort without even knowing it!
+//
+//the way it works is that we maintain two sections of the array, one with sorted items, and one with unsorted
+//items. we then go through the entire array, and find a suitable item to move over to the sorted section, until
+//the entire array is sorted.
+//
+//Example-
+//lets say I have the following cards-
+// 3   7   0   5   6
+// and I want to sort them, what would I do?
+//I'd follow this procedure-
+// first, i look through the cards to find the card with the smallest number
+//      3   7   0   5   6
+//              ^
+//           smallest
+// then, i move it to the side, as the "sorted part"
+//      0  |  3   7   5   6
+//   sorted|  unsorted    
+//
+//then, in the unsorted part, i once again look for the smallest card
+//      0  |  3   7   5   6
+//            ^
+//           smallest
+// then, i move that card to the sorted part
+//      0   3  |  7   5   6
+//
+// once again, i look for the smallest card in the unsorted part
+//      0   3  |  7   5   6
+//                    ^
+//                  smallest
+// then i move it to the sorted part
+//
+//     0   3   5  |  7   6
+//
+// now that only 2 numbers are left, i know that if i find the smallest number and move it to the sorted part,
+// the only remaining number left must be the largest number. so, without further comparisions after that, i can
+// move it to the end of the sorted array as it is the largest number
+//    0   3   5  |  7   6
+//                      ^
+//                    smallest
+//  after moving it to the sorted part, i am left with-
+//    0   3   5   6  |  7
+//  now, only one number is left in the unsorted part, so, it MUST be the largest number, and now i can move it to
+//  the end of the sorted cards.
+//
+//   0   3   5   6   7
+//  our cards are now sorted.
+//  we will follow this same approach.
+//
+// implementation-
+// 1. create nested loops i and j. i is the outer loop, which will go from 0 to length - 1. j is the inner loop,
+// which will go from i + 1 to length - 1, as in each iteration of i, the first i elements would make up the sorted
+// part.
+// 2. compare elements and accordingly swap
+//
+function selectionSort(arr: number[]): number[] {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let smallest = arr[i];
+        let smallestsIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < smallest) {
+                smallest = arr[j];
+                smallestsIndex = j;
+            }
+        }
+        let tempArrI = arr[i];
+        arr[i] = smallest;
+        arr[smallestsIndex] = tempArrI;
+    }
+    return arr;
+}
+
+//TC: O(n^2)  (due to the nested loops which both depend on length of array)
+//SC: O(1)    (memory taken does not depend on the length of array)
